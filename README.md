58e37ee280498ead4c56b3a7b47af2b24a3c4061
Geoffroy Lesage <gefthefrench@gmail.com>
2017-01-28 16:35:16 -0600

    Remove DB & make use of account info on Bitfinex

--- README.md
+++ README.md
@@ -16,6 +16,6 @@ export DRIVE_CREDS="" // JSON credentials file for drive sheet access
 export BIT_KEY="" // Bitfinex API KEY
 export BIT_SECRET="" // Bitfinex API SECRET
 
-export RISK=0.02 // Difference of price to trade at
-export FEE=0.002 // Overal aproximate fee of the exchange
+export MAX_RISK=0.02 // Max difference of price to trade at
+export MIN_RISK=0.01 // Min difference of price to trade at
 ```
--- app.js
+++ app.js
@@ -1,45 +1,98 @@
 /* Dependencies */
 var BitfinexWS = require('bitfinex-api-node');
-var TradeSheet = require('./lib/sheet');
+var Sheet = require('./lib/sheet');
 var Trader = require('./lib/trader');
+var Boot = require('./lib/boot');
 var moment = require('moment');
-var DB = require('./lib/db');
 
 
-var fee = parseFloat(process.env.FEE);
-var risk = parseFloat(process.env.RISK);
-
-var sheet = new TradeSheet(
+/**
+ * Instanciate sheets for recording & bitfinex interfaces
+ */
+var boot = new Boot();
+var sheet = new Sheet(
     process.env.DRIVE_SHEET,
-    process.env.DRIVE_CREDS,
-    process.env.SUFFIX
+    process.env.DRIVE_CREDS
 );
-var bws = new BitfinexWS(process.env.BIT_KEY, process.env.BIT_SECRET).ws;
-var trader = new Trader(risk, fee);
-var db = new DB(process.env.MONGO_URL);
+var bws = new BitfinexWS(process.env.BIT_WS_KEY, process.env.BIT_WS_SECRET, 2).ws;
+var rest = new BitfinexWS(process.env.BIT_REST_KEY, process.env.BIT_REST_SECRET).rest;
 
 
+/**
+ * Instanciate Trader Mastermind
+ */
+var risks = {
+    max: parseFloat(process.env.MAX_RISK),
+    min: parseFloat(process.env.MIN_RISK)
+};
+var fees = {
+    maker: 0.001,
+    taker: 0.002
+};
+var trader = new Trader(risks, fees);
+
+
+/**
+ * Main datastructure recording state
+ */
 var data = {
-    balanceUSD: 10000,
+    balanceUSD: 0,
     balanceBTC: 0,
-    lastBuyAt: 0,
-    lastSellAt: 0
+    lastBuy: null,
+    lastSell: null,
+    loggedOnce: false
 };
 
-restoreData();
 
 /**
  * Bitfinex socket listeners
  */
 bws.on('open', function ()
 {
-    bws.subscribeTrades('BTCUSD');
-    startTracking();
+    boot.init(rest, sheet, function (accountData, traderData, feesData)
+    {
+        if (accountData) data = accountData;
+        if (feesData) fees = feesData;
+        if (traderData && traderData.highestSupportZone > 0)
+        {
+            try
+            {
+                var sz = parseFloat(traderData.highestSupportZone);
+                if (sz !== NaN && sz > 0) trader.highestSupportZone = sz;
+            }
+            catch (e)
+            {}
+        }
+
+        bws.subscribeTrades('BTCUSD');
+        // bws.auth();
+    });
 });
 bws.on('trade', function (pair, trade)
 {
     trader.inboundTrade(trade);
     checkBuySell(trade.price);
+    logCurrentUpdates();
+});
+bws.on('os', function (data)
+{
+    if (!data || !data.length) return;
+
+    var order = {
+        id: data[0],
+        timestamp: data[4],
+        side: data[16] > 0 ? 'buy' : 'sell',
+        remaining_amount: data[6],
+        original_amount: data[7],
+        status: data[13], //  ACTIVE, EXECUTED, PARTIALLY FILLED, CANCELED
+        price: data[16],
+    };
+
+    if (order.side === 'buy') data.lastBuy = order;
+    if (order.side === 'sell') data.lastSell = order;
+
+    console.log("order");
+    console.log(order);
 });
 bws.on('error', console.error);
 
@@ -51,15 +104,13 @@ function checkBuySell(currentTicker)
 {
     if (data.balanceBTC > 0)
     {
-        if (!trader.timeToSell(currentTicker, data.lastBuyAt)) return;
+        if (data.lastBuy && data.lastBuy.is_live) return;
+        if (!trader.timeToSell(currentTicker, data.lastBuy.price)) return;
 
         trader.highestSupportZone = 0;
 
         data.balanceUSD = (data.balanceBTC * currentTicker) * (1 - fee);
         data.balanceBTC = 0;
-
-        data.lastSellAt = currentTicker;
-        data.lastBuyAt = 0;
 
         sheet.recordMyTrade(
         {
@@ -68,17 +119,19 @@ function checkBuySell(currentTicker)
             type: 'sell',
             amountUSD: data.balanceUSD,
             amountBTC: data.balanceBTC
-        }).catch(console.log);
+        }).catch(function (err)
+        {
+            console.log("Could not record sell trade");
+            console.log(err);
+        });
     }
     else if (data.balanceUSD > 0)
     {
+        if (data.lastSell && data.lastSell.is_live) return;
         if (!trader.timeToBuy(currentTicker)) return;
 
         data.balanceBTC = (data.balanceUSD / currentTicker) * (1 - fee);
         data.balanceUSD = 0;
-
-        data.lastBuyAt = currentTicker;
-        data.lastSellAt = 0;
 
         sheet.recordMyTrade(
         {
@@ -87,7 +140,11 @@ function checkBuySell(currentTicker)
             type: 'buy',
             amountUSD: data.balanceUSD,
             amountBTC: data.balanceBTC
-        }).catch(console.log);
+        }).catch(function (err)
+        {
+            console.log("Could not record buy trade");
+            console.log(err);
+        });
     }
 }
 
@@ -95,37 +152,43 @@ function checkBuySell(currentTicker)
  * Every 120 seconds log what the trader is thinking
  * for debugging purposes for now
  */
-function startTracking()
+var lastResistanceZone = 0;
+var lastSupportZone = 0;
+
+function logCurrentUpdates()
 {
-    setInterval(function ()
+    var currentData = JSON.parse(JSON.stringify(data));
+
+    currentData.supportZone = 0;
+    if (trader.highestSupportZone)
     {
-        var currentData = JSON.parse(JSON.stringify(data));
-        currentData.timestamp = Date.now();
-        currentData.supportZone = trader.highestSupportZone;
+        var sz = parseFloat(trader.highestSupportZone);
+        if (sz !== NaN && sz > 0) currentData.supportZone = sz;
+    }
 
-        db.recordTraderData(currentData).catch(console.log);
-
-        var resistanceZone = trader.lowestResistanceZone(data.lastBuyAt);
-        currentData.time = moment().format('MM/DD HH:mm:ss');
-        currentData.resistanceZone = resistanceZone;
-        sheet.recordTraderData(currentData).catch(console.log);
-    }, 120000);
-}
-
-/**
- * Utility to restore the session data using backup in MongoDB
- */
-function restoreData()
-{
-    db.getLastTraderData(null, ['timestamp'], 1).then(function (traderData)
+    currentData.resistanceZone = 0;
+    if (data.lastBuy)
     {
-        if (!traderData) return;
+        currentData.resistanceZone = trader.lowestResistanceZone(data.lastBuy.price);
+    }
 
-        data.balanceUSD = parseFloat(traderData.balanceUSD) || 0;
-        data.balanceBTC = parseFloat(traderData.balanceBTC) || 0;
-        data.lastBuyAt = parseFloat(traderData.lastBuyAt) || 0;
-        data.lastSellAt = parseFloat(traderData.lastSellAt) || 0;
+    if (data.loggedOnce &&
+        (currentData.resistanceZone === lastResistanceZone &&
+            currentData.supportZone === lastSupportZone)) return;
 
-        trader.highestSupportZone = parseFloat(traderData.supportZone) || 0;
+    data.loggedOnce = true;
+
+    delete currentData.lastBuy;
+    delete currentData.lastSell;
+
+    lastResistanceZone = currentData.resistanceZone;
+    lastSupportZone = currentData.supportZone;
+
+    currentData.time = moment().format('MM/DD HH:mm:ss');
+
+    sheet.recordTraderData(currentData).catch(function (err)
+    {
+        console.log("Could not record trader data");
+        console.log(err);
     });
 }
